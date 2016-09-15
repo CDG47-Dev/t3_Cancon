bbb24f06f0aacdb0df3a260742163615678d0b71
CDG47 <n8F6NHoW>
2016-09-15 11:33:20 +0200

    Ajout déclaration de constantes
    Permet la modification des variables dans l'éditeur de Constantes

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,7 +1,7 @@
 <?php
 
 /***************************************************************
- * Extension Manager/Repository config file for ext "pradinestemplate".
+ * Extension Manager/Repository config file for ext "t3_Cancon".
  *
  * Auto generated 23-08-2014 13:00
  *
@@ -17,7 +17,9 @@ $EM_CONF[$_EXTKEY] = array(
         'depends' => array(
             'bootstrap_package' => '7.0.2 - 8.99.99',
             'typo3' => '7.6.2 - 8.99.99',
-	    'indexed_search' => '7.6.0 - 8.99.99',
+			'indexed_search' => '7.6.0 - 8.99.99',
+			'news' => '5.0.0 - 5.9.99',			
+			'dd_googlesitemap' => '*',
 			
         ),
         'conflicts' => array(),
@@ -26,9 +28,9 @@ $EM_CONF[$_EXTKEY] = array(
     'uploadfolder' => 1,
     'createDirs' => '',
     'clearCacheOnLoad' => 1,
-    'author' => 'Hediard',
-    'author_email' => 'christophe.hediard@outlook.fr',
+    'author' => 'C.Hediard, O. Pommaaret',
+    'author_email' => 'services.internet@cdg47.fr',
     'author_company' => 'CDG47',
-    'version' => '7.6.1',
+    'version' => '7.6.2',
 );
 ?>
